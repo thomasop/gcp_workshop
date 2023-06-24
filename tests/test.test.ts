25bba0df5aaeee1b76b32bb52218e5e4eaa4fad7
thomasop <tdss33@hotmail.com>
2023-06-24 13:53:09 +0200

    Add: express route

--- tests/test.test.ts
+++ tests/test.test.ts
@@ -1,5 +1,15 @@
-describe("test suite", () => {
-    it("test case", () => {
-      expect(true).toBe(true);
-    });
+import app, { server } from "../src/index";
+import request from "supertest";
+
+describe("GET /", () => {
+  it("should return 200 OK", async () => {
+    const response = await request(app).get("/");
+    expect(response.status).toEqual(200);
+    expect(response.body.data).toBeNull();
+    expect(response.body.msg).toEqual("API is up");
   });
+});
+
+afterAll(() => {
+  server.close();
+});
