80d398ce6e996498c185dd93efba9bc0f31ce1f8
thomasop <tdss33@hotmail.com>
2023-07-25 07:19:02 +0200

    edit

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
 import { server } from "./app";
+import dotenv from "dotenv";
+dotenv.config();
 
 let port = 8080;
 
-server.listen(port, () => {
+server.listen(process.env.PORT, () => {
   console.log("App listen on port 8080");
 });
